ea9ef70cc6ae9e28c1b0d86c99f99ba267f9b1fd
squareGTS <68609996+squareGTS@users.noreply.github.com>
2020-09-14 11:32:55 +0300

    ByteCoin
    Add Picker and IBOutlets

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -8,12 +8,17 @@
 
 import UIKit
 
-protocol UIPickerViewDataSource {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     
-}
-
-
-class ViewController: UIViewController, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1 // how many columns in picker
+    }
+    
+    let coinManager = CoinManager()
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count //how many rows in picker
+    }
+    
     @IBOutlet weak var bitcoinLabel: UILabel!
     @IBOutlet weak var currencyLabel: UILabel!
     @IBOutlet weak var currencyPicker: UIPickerView!
@@ -21,8 +26,20 @@ class ViewController: UIViewController, UIPickerViewDataSource {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        currencyPicker.dataSource = self // ViewController class is the datasource to the currencyPicker object
+        currencyPicker.delegate = self
     }
-
-
+    
+    //use the row (Int) to pick the title from our currencyArray
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+    }
+    
+    //called every time when the user scrolls the picker
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedCurrency = coinManager.currencyArray[row]
+        coinManager.getCoinPrice(for: selectedCurrency)
+    }
 }
 
